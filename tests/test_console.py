7c6e202454bf81bb65c85bfea9539ac0abc24172
Ahmed Hosny <ahmhosny2002@gmail.com>
2023-12-11 02:38:48 +0200

    Unittest for console is created.

---
+++ tests/test_console.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Defines unittests for console.py."""
+
+import unittest
+from console import HBNBCommand
+from unittest.mock import patch
+from io import StringIO
+
+
+class HBNBCommand_prompting(unittest.TestCase):
+    def test_prompt_string(self):
+        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
+
+    def test_empty_line(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd(""))
+            self.assertEqual("", output.getvalue().strip())
+
+
+class HBNBCommand_exit(unittest.TestCase):
+    """Unittests for testing exiting from the HBNB command interpreter."""
+
+    def test_quit_exits(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertTrue(HBNBCommand().onecmd("quit"))
+
+    def test_EOF_exits(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertTrue(HBNBCommand().onecmd("EOF"))
+
+
+if __name__ == '__main__':
+    unittest.main()
